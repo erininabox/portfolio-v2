9c71e071c321cce3f84458d5593d020c81787f62
erininabox <erininabox@gmail.com>
2022-09-30 16:18:13 -0700

    GA fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
+
   return (
   <>
     <Head>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,6 +3,7 @@ import Script from 'next/script'
 
 
 export default function Document() {
+  
   return (
     <Html lang="en">
       <Head>
@@ -10,13 +11,17 @@ export default function Document() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Script async src="https://www.googletagmanager.com/gtag/js?id=G-X0J5R98E7C" strategy="afterInteractive"></Script>
-      <Script id="google-analytics" strategy="afterInteractive">
+      <Script id="google-analytics" strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
+          function gtag(){dataLayer.push(arguments)};
           gtag('js', new Date());
-
-          gtag('config', 'G-X0J5R98E7C');
-      </Script>
+          gtag('config', 'G-X0J5R98E7C', {page_path: window.location.pathname,});
+        });
+        `,
+        }}
+        />
       <script src="https://kit.fontawesome.com/35eb533387.js" crossOrigin="anonymous" strategy='beforeInteractive' async />
 
       <body className='body__dark'>
