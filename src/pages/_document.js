dc053a83b20c4403f2c081edc0e61d2030440cbd
erininabox <erininabox@gmail.com>
2022-09-30 15:13:16 -0700

    next GA fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 
 export default function Document() {
@@ -8,15 +9,17 @@ export default function Document() {
         <meta name="Erin Halden Portfolio" content="" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-X0J5R98E7C"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments)}
-        gtag('js', new Date());
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-X0J5R98E7C" strategy="afterInteractive"></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
 
-        gtag('config', 'G-X0J5R98E7C');
-      </script>
-      <script src="https://kit.fontawesome.com/35eb533387.js" crossOrigin="anonymous" strategy='beforeInteractive' async />
+          gtag('config', 'G-X0J5R98E7C');
+        `}
+      </Script>
+      <Script src="https://kit.fontawesome.com/35eb533387.js" crossOrigin="anonymous" strategy='beforeInteractive' async />
 
       <body className='body__dark'>
         <Main />
