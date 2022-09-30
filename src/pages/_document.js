a6af77ca95b81ab928fbcb4c524a328003555005
erininabox <erininabox@gmail.com>
2022-09-30 15:58:12 -0700

    GA fix again

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,13 +11,11 @@ export default function Document() {
       </Head>
       <Script async src="https://www.googletagmanager.com/gtag/js?id=G-X0J5R98E7C" strategy="afterInteractive"></Script>
       <Script id="google-analytics" strategy="afterInteractive">
-        {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments)}
           gtag('js', new Date());
 
           gtag('config', 'G-X0J5R98E7C');
-        `}
       </Script>
       <script src="https://kit.fontawesome.com/35eb533387.js" crossOrigin="anonymous" strategy='beforeInteractive' async />
 
