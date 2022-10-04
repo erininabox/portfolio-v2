fd7d97634f4648ebd51f2fa0a65e19e562692752
erininabox <erininabox@gmail.com>
2022-10-04 12:26:43 -0700

    GA

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,29 @@
 import '../styles/app.scss';
 import React from 'react';
 import Head from 'next/head';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
 
   return (
   <>
+    <Script
+      strategy="lazyOnload"
+      src={`https://www.googletagmanager.com/gtag/js?id=G-G7D4BLKJVM`}
+    />
+    <Script
+      id="google-analytics"
+      strategy="lazyOnload"
+    >
+      {`window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-G7D4BLKJVM');
+      `}
+
+
+    </Script>
+    
     <Head>
       <title>Erin Halden&apos;s Portfolio</title>
     </Head>
