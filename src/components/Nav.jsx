5a81989b775b8bf92ba1858e4654bd5e85496ab8
erininabox <erininabox@gmail.com>
2022-10-19 17:07:58 -0700

    removed unused nav code

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,11 +12,6 @@ const Nav = () => {
 
   return (
     <div className='nav'>
-    <input type="checkbox" id="nav__checkbox" className="nav__checkbox" />
-      <label htmlFor="nav__checkbox" className="nav__toggle">
-        <i className="fa-solid fa-bars menu"></i>
-        <i className="fa-solid fa-xmark close"></i>
-      </label>
       
       <ol className='nav__ol'>
         <li><a href="https://erininabox.com/#about" className='nav__a' onClick={handleOnClick}>About</a></li>
